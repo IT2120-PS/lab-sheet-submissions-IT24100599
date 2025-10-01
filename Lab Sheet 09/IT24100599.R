e8c9b0adcf02f59c81b4105aeb695d03d32864a2
IT24100599 <IT24100599@my.sliit.lk>
2025-10-01 22:05:50 +0530

    Lab 9

---
+++ Lab Sheet 09/IT24100599.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\Vinod Madhuranga\\Desktop\\IT24100599")
+
+# Exercise
+
+# i.
+set.seed(123)   
+baking_time <- rnorm(25, mean = 45, sd = 2)
+print(baking_time)
+
+
+# ii.
+t_test_result <- t.test(baking_time, mu = 46, alternative = "less")
+print(t_test_result)
