496f462eef2a63aeca57cd220b3e9430d636f1cb
IT24100599 <IT24100599@my.sliit.lk>
2025-10-16 20:18:40 +0530

    Lab 10

---
+++ Lab Sheet 10/IT24100599.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\Vinod Madhuranga\\Desktop\\IT24100599")
+
+#Excercise
+
+#i
+observed_counts <- c(120, 95, 85, 100)
+
+
+#ii
+probabilities <- c(0.25, 0.25, 0.25, 0.25)
+
+#iii
+chisq.test(x = observed_counts, p = probabilities)
+
